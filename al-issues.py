d3748d14514a91d59e82d3c01e521e50f3169184
Edo de Roo <git@edoderoo.nl>
2023-02-17 23:44:35 +0100

    write file in home folder

--- al-issues.py
+++ al-issues.py
@@ -1,5 +1,6 @@
 import os
 from glob import glob
+import tempfile
 
 def getRecursiveFiles(path, filefilter):
   return [y for x in os.walk(path) for y in glob(os.path.join(x[0], filefilter))]
@@ -11,10 +12,11 @@ def CheckToolTip(line, filename, linenr):
       while line[tooltip_cursor] != '\'':
         tooltip_cursor+=1
       endpos = tooltip_cursor+1
-      while line[endpos] != '\'': 
+      while (line[endpos] != '\'') and (line[endpos-1]!='\\') and (line[endpos+1] != '\''): 
         endpos += 1
       if (line[endpos-1] != '.') :
         changed = True
+        line = line[:endpos]+'.'+line[endpos:]
         print('%s|%i|%s' % (filename, linenr, line[tooltip_cursor+1:endpos]))
     return changed    
 
@@ -23,9 +25,17 @@ def procesALfile(filename):
   linenr = 0
   alFile = open(filename, 'r')
   alLines = alFile.readlines()
+  # tf = tempfile.NamedTemporaryFile(mode='w', encoding='utf8')
+  tf = open(os.path.expanduser('~') + '/edo.txt','w')
+
+  changed = False
   for oneline in alLines:
     linenr += 1
-    CheckToolTip(oneline, filename, linenr)
+    changed = changed or CheckToolTip(oneline, filename, linenr)
+    tf.write(oneline)
+  print(tf.name)
+  # os.rename(tf.name, '/tmp/edo.txt')
+  tf.close
 
 # files = getRecursiveFiles('/media/edo/Linux Home/Downloads/ISF-AL/App/iSFB','*.al')
 # for filename in files:
