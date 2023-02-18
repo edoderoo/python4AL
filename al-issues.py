3d0f2701c881d917e9fc717a6532a0a2d747b412
Edo de Roo <git@edoderoo.nl>
2023-02-18 17:54:55 +0100

    missing a copy/rename

--- al-issues.py
+++ al-issues.py
@@ -5,18 +5,32 @@ import tempfile
 def getRecursiveFiles(path, filefilter):
   return [y for x in os.walk(path) for y in glob(os.path.join(x[0], filefilter))]
 
+def charoussel(p,t,n,nextchar):
+ p = t
+ t = n
+ n = nextchar
+ return p,t,n
+
 def CheckToolTip(line, filename, linenr):
+    if len(line)<8: return False
     changed = False
+    orgline=line
     tooltip_cursor = line.lower().find('tooltip')
     if tooltip_cursor>0:
       while line[tooltip_cursor] != '\'':
         tooltip_cursor+=1
       endpos = tooltip_cursor+1
-      while (line[endpos] != '\'') and (line[endpos-1]!='\\') and (line[endpos+1] != '\''): 
+      prevchar=line[tooltip_cursor-1]
+      prevchar='\\'
+      thischar=line[tooltip_cursor]
+      nextchar=line[tooltip_cursor+1]
+      while ((thischar != '\'') or ((thischar == '\'') and (     (prevchar=='\'') or (nextchar=='\'') or (prevchar=='\\')     )   )):
         endpos += 1
-      if (line[endpos-1] != '.') :
+        prevchar,thischar,nextchar=charoussel(prevchar,thischar,nextchar,line[endpos])
+
+      if (prevchar != '.') :
         changed = True
-        line = line[:endpos]+'.'+line[endpos:]
+        line = line[:endpos-1]+'.'+line[endpos-1:]
         print('%s|%i|%s' % (filename, linenr, line[tooltip_cursor+1:endpos]))
     return changed    
 
@@ -26,16 +40,17 @@ def procesALfile(filename):
   alFile = open(filename, 'r')
   alLines = alFile.readlines()
   # tf = tempfile.NamedTemporaryFile(mode='w', encoding='utf8')
-  tf = open(os.path.expanduser('~') + '/edo.txt','w')
 
   changed = False
   for oneline in alLines:
+    tf = open(os.path.expanduser('~') + '/todo-al.txt','w')
     linenr += 1
     changed = changed or CheckToolTip(oneline, filename, linenr)
     tf.write(oneline)
-  print(tf.name)
-  # os.rename(tf.name, '/tmp/edo.txt')
-  tf.close
+  if changed:
+    tf.close
+    os.copy_file_range(tf.name, '/media/edo/Linux Home/Downloads/ISF-AL/App/iSFB/pageext/LocationCardISFB2.PageExt.al',1)
+    print(tf.name)
 
 # files = getRecursiveFiles('/media/edo/Linux Home/Downloads/ISF-AL/App/iSFB','*.al')
 # for filename in files:
