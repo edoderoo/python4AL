03bb9072e6db9bcaa187c8ef7ec8142ffe540bff
Edo de Roo <git@edoderoo.nl>
2023-02-19 12:20:36 +0100

    ready to test first time

--- al-issues.py
+++ al-issues.py
@@ -1,6 +1,9 @@
 import os
+import shutil
 from glob import glob
 import tempfile
+
+global bufferLines
 
 def getRecursiveFiles(path, filefilter):
   return [y for x in os.walk(path) for y in glob(os.path.join(x[0], filefilter))]
@@ -11,8 +14,8 @@ def charoussel(p,t,n,nextchar):
  n = nextchar
  return p,t,n
 
-def CheckToolTip(line, filename, linenr):
-    if len(line)<8: return False
+def CheckToolTipOnThisLine(line, filename, linenr):
+    if len(line)<8: return False, line
     changed = False
     orgline=line
     tooltip_cursor = line.lower().find('tooltip')
@@ -31,28 +34,52 @@ def CheckToolTip(line, filename, linenr):
       if (prevchar != '.') :
         changed = True
         line = line[:endpos-1]+'.'+line[endpos-1:]
-        print('%s|%i|%s' % (filename, linenr, line[tooltip_cursor+1:endpos]))
-    return changed    
+        #print('%s|%i|%s' % (filename, linenr, line[tooltip_cursor+1:endpos]))
+    return changed, line    
 
+def mycopy(namein, nameout): #std move/copy only gave zero byte files
+  fin=open(namein, 'r')
+  fout=open(nameout, 'w')
+  lines=fin.readlines()
+  for oneline in lines:
+    fout.write(oneline)
+  fin.close
+  fout.close
 
 def procesALfile(filename):
   linenr = 0
+  bufferLines=[]
   alFile = open(filename, 'r')
   alLines = alFile.readlines()
   # tf = tempfile.NamedTemporaryFile(mode='w', encoding='utf8')
 
-  changed = False
+  allchanged = False
+  newchanged = False
   for oneline in alLines:
-    tf = open(os.path.expanduser('~') + '/todo-al.txt','w')
     linenr += 1
-    changed = changed or CheckToolTip(oneline, filename, linenr)
-    tf.write(oneline)
-  if changed:
-    tf.close
-    os.copy_file_range(tf.name, '/media/edo/Linux Home/Downloads/ISF-AL/App/iSFB/pageext/LocationCardISFB2.PageExt.al',1)
-    print(tf.name)
+    newchanged, newline = CheckToolTipOnThisLine(oneline, filename, linenr)
+    allchanged = allchanged or newchanged
+    bufferLines.append(newline)
+    
+  alFile.close
+  #tf.flush
+  #os.fsync(tf.fileno())
+  #tf.close
+  if allchanged:
+    #mycopy(tf.name, os.path.expanduser('~') + '/done-al.txt')
+    #shutil.move(tf.name, '/media/edo/Linux Home/Downloads/ISF-AL/App/iSFB/pageext/die.PageExt.al')
+    #print(tf.name)
+    alFile = open(filename,'w')
+    #alFile = open('/media/edo/Linux Home/Downloads/ISF-AL/App/iSFB/pageext/die.PageExt.al', 'w')
+    for oneline in bufferLines:
+      #print(oneline)
+      alFile.write(oneline)
+    alFile.close  
+    bufferLines=[]
 
-# files = getRecursiveFiles('/media/edo/Linux Home/Downloads/ISF-AL/App/iSFB','*.al')
-# for filename in files:
-#   procesALfile(filename)
-procesALfile('/media/edo/Linux Home/Downloads/ISF-AL/App/iSFB/pageext/LocationCardISFB.PageExt.al')
+
+files = getRecursiveFiles('/media/edo/Linux Home/Downloads/iSF-AL/App/iSFB/','*.al')
+for filename in files:
+  procesALfile(filename)
+  #print(filename)
+#procesALfile('/media/edo/Linux Home/Downloads/ISF-AL/App/iSFB/pageext/deze.PageExt.al')
