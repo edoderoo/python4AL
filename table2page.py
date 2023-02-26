e3cddf7d78850fad273603f7c8e590e61e19fa77
Edo de Roo <git@edoderoo.nl>
2023-02-26 20:45:01 +0100

    table2page

--- table2page.py
+++ table2page.py
@@ -6,16 +6,25 @@ import tempfile
 def getRecursiveFiles(path, filefilter):
   return [y for x in os.walk(path) for y in glob(os.path.join(x[0], filefilter))]
 
-def OneTable2Page(tablefile):
-  #pagefile = open('', 'w')
-  path,filename = os.path.split(tablefile)
-  print(path)
-  print(filename)
+def OneTable2Page(tablefilename):
+  path,filename = os.path.split(tablefilename)
+  pagefilename = filename.lower().replace('.table.al','.page.al')
+  pagepath=os.path.join(path,pagefilename)
+  tablefile=open(tablefilename,'r')
+  tablelines=tablefile.readlines()
+  for tableline in tablelines:
+    print(tableline)
+    
+  # pagefile=open(pagefilename,'w')
+
+  tablefile.close()
+  # close(pagefile)
 
 
 
-# files = getRecursiveFiles('/media/edo/Linux Home/Downloads/iSF-AL/App/iSFB/','*.Table.al')
-# for filename in files:
-#   OneTable2Page(filename)
-
-OneTable2Page('/media/edo/Linux Home/Downloads/iSF-AL/App/iSFB/obsolete/PrepackDefinitionISFB.Table.al')
+if False:
+  files = getRecursiveFiles('/media/edo/Linux Home/Downloads/iSF-AL/App/iSFB/','*.?able.al')
+  for filename in files:
+    OneTable2Page(filename)
+else:
+  OneTable2Page('/media/edo/Linux Home/Downloads/iSF-AL/App/iSFB/obsolete/PrepackDefinitionISFB.Table.al')
